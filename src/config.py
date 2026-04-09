528896261b486577eb30cf13c9d4089ab75dd3aa
cylb0 <foucaut.morgan@gmail.com>
2026-04-09 14:26:06 +0200

    refactor: convert Config to an instantiated object and inject into main, and add pytest configuration with placeholder test to avoid exit 5 error

--- src/config.py
+++ src/config.py
@@ -4,11 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    S3_BUCKET: str = os.getenv("S3_BUCKET_NAME")
-    MANIFEST_PATH: str = os.getenv("MANIFEST_PATH", "manifest.json")
-    TMP_DIR: str = os.getenv("TMP_DIR", "tmp")
+    def __init__(self):
+        self.S3_BUCKET: str = os.getenv("S3_BUCKET_NAME")
+        self.MANIFEST_PATH: str = os.getenv("MANIFEST_PATH", "manifest.json")
+        self.TMP_DIR: str = os.getenv("TMP_DIR", "tmp")
 
-    RESOURCES: dict[str, str] = {
+        self.RESOURCES: dict[str, str] = {
         "parts": os.getenv("REBRICKABLE_PARTS_CSV_URL"),
         "categories": os.getenv("REBRICKABLE_CATEGORIES_CSV_URL"),
     }
+
+settings = Config()
