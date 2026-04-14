e3498a51ca170918be61801b69917b2e409f264e
cylb0 <foucaut.morgan@gmail.com>
2026-04-14 14:02:44 +0200

    fix: refactor pipeline code, removed redundance

--- src/core/pipeline.py
+++ src/core/pipeline.py
@@ -27,12 +27,14 @@ class CatalogPipeline:
         logger.info("Starting catalog pipeline run")
 
         downloads = self.csv_downloader.fetch_data()
-
         for resource, data in downloads.items():
-            self._sync_resource(resource, data)
+            if self.s3_manager.check_for_resource_changes(resource, data["hash"]):
+                logger.info(f"Changes detected for {resource}, uploading to S3")
+                self._sync_resource(resource, data)
+            else:
+                logger.info(f"No change detected for {resource}")
 
         latest_ldraw_date = self.ldraw_downloader.get_latest_version_date()
-
         if self.s3_manager.check_for_ldraw_changes(latest_ldraw_date):
             logger.info("New LDraw library detected, downloading and uploading to S3")
             local_ldraw = self.ldraw_downloader.fetch_library()
@@ -46,15 +48,8 @@ class CatalogPipeline:
 
     def _sync_resource(self, resource: str, data: dict):
         """
-        Sync a resource to S3 if it has changed.
-        :param resource: The name of the resource to sync
-        :param data: The data to sync
+        Sync a resource to S3.
         """
-        if not self.s3_manager.check_for_resource_changes(resource, data["hash"]):
-            logger.info(f"No changes detected for {resource}")
-            return
-
-        logger.info(f"Changes detected for {resource}, uploading to S3")
         old_filename = self.s3_manager.get_csv_filename(resource)
 
         if self.s3_manager.upload_to_s3(data["local_path"], data["filename"]):
@@ -66,6 +61,9 @@ class CatalogPipeline:
         logger.info(f"Resource {resource} synced successfully")
 
     def _sync_ldraw(self, local_ldraw: str, remote_date: str):
+        """
+        Sync LDraw library to S3.
+        """
         if not self.s3_manager.check_for_ldraw_changes(remote_date):
             logger.info("No changes detected for LDraw library")
             return
