a388af480458a38af2465cffb28168d3feb520a6
cylb0 <foucaut.morgan@gmail.com>
2026-04-22 14:14:24 +0200

    fix: CLOUDWATCH env in run_container script

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run --rm -it \
     -e AWS_PROFILE=lego-worker \
-    -e CLOUDWATCH_LOG_GROUP=lego-catalog-pipeline \
+    -e CLOUDWATCH_LOG_GROUP=lego-catalog-pipeline-dev \
     -e S3_BUCKET_NAME=lego-catalog-storage \
     -e SQS_QUEUE_URL=https://sqs.eu-west-3.amazonaws.com/730335557058/lego-catalog-queue \
     -e LDRAW_PATH=/tmp/ldraw.zip \
@@ -9,3 +9,4 @@ docker run --rm -it \
     -e MAX_EMPTY_POLLS=3 \
     -v ~/.aws:/root/.aws \
     lego-converter
+    
